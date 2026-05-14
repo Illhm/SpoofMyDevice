134f02c6341993eefae3ebfccac9e912b8a8f8ef
Illhm <194127535+Illhm@users.noreply.github.com>
2026-05-14 19:58:48 +0000

    feat: add random all button and 50 Samsung presets\n\n- Add a 'Random all' button to the device settings layout, alongside the existing 'Clear all' button, which generates random spoof values for all advanced fields (IMEI, MEID, IMSI, ICCID, Phone Number, GAID, GSF ID, Media DRM ID, App Set ID).\n- Add a `SamsungPresets` utility class that registers 50 Samsung device models into the preset catalog.

---
+++ app/src/main/java/com/devicespooflab/hooks/data/SamsungPresets.java
@@ -0,0 +1,80 @@
+package com.devicespooflab.hooks.data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SamsungPresets {
+
+    public static List<DevicePreset> getPresets() {
+        List<DevicePreset> presets = new ArrayList<>();
+
+        presets.add(createSamsungPreset("Galaxy S24 Ultra", "SM-S928B", "e3q", "qcom", "14", 34));
+        presets.add(createSamsungPreset("Galaxy S24+", "SM-S926B", "e2q", "qcom", "14", 34));
+        presets.add(createSamsungPreset("Galaxy S24", "SM-S921B", "e1q", "qcom", "14", 34));
+        presets.add(createSamsungPreset("Galaxy S23 Ultra", "SM-S918B", "kalama", "qcom", "13", 33));
+        presets.add(createSamsungPreset("Galaxy S23+", "SM-S916B", "kalama", "qcom", "13", 33));
+        presets.add(createSamsungPreset("Galaxy S23", "SM-S911B", "kalama", "qcom", "13", 33));
+        presets.add(createSamsungPreset("Galaxy S22 Ultra", "SM-S908B", "taro", "qcom", "12", 32));
+        presets.add(createSamsungPreset("Galaxy S22+", "SM-S906B", "taro", "qcom", "12", 32));
+        presets.add(createSamsungPreset("Galaxy S22", "SM-S901B", "taro", "qcom", "12", 32));
+        presets.add(createSamsungPreset("Galaxy S21 Ultra", "SM-G998B", "exynos2100", "exynos", "11", 30));
+        presets.add(createSamsungPreset("Galaxy S21+", "SM-G996B", "exynos2100", "exynos", "11", 30));
+        presets.add(createSamsungPreset("Galaxy S21", "SM-G991B", "exynos2100", "exynos", "11", 30));
+        presets.add(createSamsungPreset("Galaxy S20 Ultra", "SM-G988B", "exynos990", "exynos", "10", 29));
+        presets.add(createSamsungPreset("Galaxy S20+", "SM-G986B", "exynos990", "exynos", "10", 29));
+        presets.add(createSamsungPreset("Galaxy S20", "SM-G981B", "exynos990", "exynos", "10", 29));
+        presets.add(createSamsungPreset("Galaxy Z Fold5", "SM-F946B", "kalama", "qcom", "13", 33));
+        presets.add(createSamsungPreset("Galaxy Z Fold4", "SM-F936B", "taro", "qcom", "12", 32));
+        presets.add(createSamsungPreset("Galaxy Z Fold3", "SM-F926B", "lahaina", "qcom", "11", 30));
+        presets.add(createSamsungPreset("Galaxy Z Flip5", "SM-F731B", "kalama", "qcom", "13", 33));
+        presets.add(createSamsungPreset("Galaxy Z Flip4", "SM-F721B", "taro", "qcom", "12", 32));
+        presets.add(createSamsungPreset("Galaxy Z Flip3", "SM-F711B", "lahaina", "qcom", "11", 30));
+        presets.add(createSamsungPreset("Galaxy A54 5G", "SM-A546B", "s5e8835", "exynos", "13", 33));
+        presets.add(createSamsungPreset("Galaxy A53 5G", "SM-A536B", "s5e8825", "exynos", "12", 32));
+        presets.add(createSamsungPreset("Galaxy A52s 5G", "SM-A528B", "lahaina", "qcom", "11", 30));
+        presets.add(createSamsungPreset("Galaxy A52 5G", "SM-A526B", "lito", "qcom", "11", 30));
+        presets.add(createSamsungPreset("Galaxy A52", "SM-A525F", "atoll", "qcom", "11", 30));
+        presets.add(createSamsungPreset("Galaxy A34 5G", "SM-A346B", "mt6877", "mt6877", "13", 33));
+        presets.add(createSamsungPreset("Galaxy A33 5G", "SM-A336B", "s5e8825", "exynos", "12", 32));
+        presets.add(createSamsungPreset("Galaxy A73 5G", "SM-A736B", "lahaina", "qcom", "12", 32));
+        presets.add(createSamsungPreset("Galaxy A72", "SM-A725F", "atoll", "qcom", "11", 30));
+        presets.add(createSamsungPreset("Galaxy M54 5G", "SM-M546B", "s5e8835", "exynos", "13", 33));
+        presets.add(createSamsungPreset("Galaxy M53 5G", "SM-M536B", "mt6877", "mt6877", "12", 32));
+        presets.add(createSamsungPreset("Galaxy M34 5G", "SM-M346B", "s5e8825", "exynos", "13", 33));
+        presets.add(createSamsungPreset("Galaxy M33 5G", "SM-M336B", "s5e8825", "exynos", "12", 32));
+        presets.add(createSamsungPreset("Galaxy F54 5G", "SM-E546B", "s5e8835", "exynos", "13", 33));
+        presets.add(createSamsungPreset("Galaxy F14 5G", "SM-E146B", "s5e8535", "exynos", "13", 33));
+        presets.add(createSamsungPreset("Galaxy Note20 Ultra", "SM-N986B", "exynos990", "exynos", "10", 29));
+        presets.add(createSamsungPreset("Galaxy Note20", "SM-N981B", "exynos990", "exynos", "10", 29));
+        presets.add(createSamsungPreset("Galaxy Note10+", "SM-N975F", "exynos9825", "exynos", "9", 28));
+        presets.add(createSamsungPreset("Galaxy Note10", "SM-N970F", "exynos9825", "exynos", "9", 28));
+        presets.add(createSamsungPreset("Galaxy S10+", "SM-G975F", "exynos9820", "exynos", "9", 28));
+        presets.add(createSamsungPreset("Galaxy S10", "SM-G973F", "exynos9820", "exynos", "9", 28));
+        presets.add(createSamsungPreset("Galaxy S10e", "SM-G970F", "exynos9820", "exynos", "9", 28));
+        presets.add(createSamsungPreset("Galaxy A14 5G", "SM-A146B", "mt6833", "mt6833", "13", 33));
+        presets.add(createSamsungPreset("Galaxy A13 5G", "SM-A136B", "mt6833", "mt6833", "12", 32));
+        presets.add(createSamsungPreset("Galaxy A24", "SM-A245F", "mt6789", "mt6789", "13", 33));
+        presets.add(createSamsungPreset("Galaxy A23 5G", "SM-A236B", "holi", "qcom", "12", 32));
+        presets.add(createSamsungPreset("Galaxy Tab S9 Ultra", "SM-X910", "kalama", "qcom", "13", 33));
+        presets.add(createSamsungPreset("Galaxy Tab S9+", "SM-X810", "kalama", "qcom", "13", 33));
+        presets.add(createSamsungPreset("Galaxy Tab S9", "SM-X710", "kalama", "qcom", "13", 33));
+
+        return presets;
+    }
+
+    private static DevicePreset createSamsungPreset(String modelName, String deviceCode, String board, String hardware, String release, int sdk) {
+        DeviceProfile profile = new DeviceProfile();
+        profile.setBrand("samsung");
+        profile.setManufacturer("samsung");
+        profile.setModel(deviceCode);
+        profile.setProductName(board);
+        profile.setDeviceCode(board);
+        profile.setBoard(board);
+        profile.setHardware(hardware);
+        profile.setBoardPlatform(hardware);
+        profile.setBuildRelease(release);
+        profile.setBuildSdk(sdk);
+        profile.applyFallbacks();
+        return new DevicePreset("samsung_" + deviceCode.toLowerCase(), "Samsung", modelName, "Android " + release, profile);
+    }
+}
