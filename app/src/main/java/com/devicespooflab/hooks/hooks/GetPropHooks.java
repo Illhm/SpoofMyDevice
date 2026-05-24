76d346664f52eb22e6ea848b060f215b8d04699b
Illhm <gusmif@ilhamxander.web.id>
2026-05-24 17:58:17 +0700

    Fix GetPropHooks missing Locale import for release build

--- app/src/main/java/com/devicespooflab/hooks/hooks/GetPropHooks.java
+++ app/src/main/java/com/devicespooflab/hooks/hooks/GetPropHooks.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import de.robv.android.xposed.XC_MethodHook;
